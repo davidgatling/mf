d9b52eb3be0384e15d6829c925f517e2b585f6fc
iamgatling <95552807+davidgatling@users.noreply.github.com>
2023-03-06 22:22:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 function myFunction() {
-  // Get the text field
-  var copyText = document.getElementById("myInput");
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
 
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value)
-    .then(() => {
-      // Show a toast message when the text is copied successfully
-      var toast = document.getElementById("toast");
-      toast.className = "show";
-      setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
-    })
-    .catch(() => {
-      // If copying text fails, show an error message
-      alert("Copying text failed!");
-    });
-}
